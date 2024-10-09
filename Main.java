ae184ab3543cffadb809d82019519ca39d3676f9
Rudra Pratap Singh <130541940+rudrapratap63@users.noreply.github.com>
2024-10-09 11:07:04 +0530

    add

--- Main.java
+++ Main.java
@@ -3,11 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-         User user = new Faculty("Rudra","22Egjcs182");
-         user.getInfo();
-         User user1 = new Student("Rudra","1234");
-         user1.getInfo();
-
+        Pass pass = new Monthly();
+        pass.priceDecider("cycle", "monthly");
     }
 
 }
